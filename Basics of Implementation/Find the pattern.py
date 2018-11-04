e02ea1c17e546fe54e5b707cdf85dcd07c8eb66e
NileshSuthar <nsnilesh071@gmail.com>
2018-11-04 16:56:35 +0530

    Update Find the pattern.py

--- Basics of Implementation/Find the pattern.py
+++ Basics of Implementation/Find the pattern.py
@@ -1,1 +1,12 @@
-
+n = int(input())
+p = input()
+p = p.split(" ")
+min = p[0]
+max = p[0]
+for i in range(n):
+    if p[i] < min:
+        min = p[i]
+for i in range(n):
+    if p[i] > max:
+        max = p[i]
+print(min+max)
